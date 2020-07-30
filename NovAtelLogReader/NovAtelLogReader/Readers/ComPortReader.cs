319e8963f8769d7b29fd55ffd168a1478e524b85
ioaznnis <1431061+ioaznnis@users.noreply.github.com>
2020-07-31 00:31:55 +0300

    Fix for rs-242

--- NovAtelLogReader/NovAtelLogReader/Readers/ComPortReader.cs
+++ NovAtelLogReader/NovAtelLogReader/Readers/ComPortReader.cs
@@ -81,17 +81,23 @@ namespace NovAtelLogReader.Readers
             _serialPort = new PISerialPort();
             _serialPort.Open(portName, Properties.Settings.Default.ComPortSpeed);
 #else
-            _serialPort = new SerialPort(portName);
-            _serialPort.ReadTimeout = 1500;
-            _serialPort.WriteTimeout = 1500;
-            _serialPort.DtrEnable = true;
-            _serialPort.RtsEnable = true;
-            _serialPort.BaudRate = Properties.Settings.Default.ComPortSpeed;
+            _serialPort = new SerialPort(portName)
+            {
+                ReadTimeout = 1500,
+                WriteTimeout = 1500,
+                DtrEnable = true,
+                RtsEnable = true,
+                BaudRate = Properties.Settings.Default.ComPortSpeed,
+            };
             _serialPort.Open();
 #endif
 
             _logger.Info("Инициализация приемника");
-            Properties.Settings.Default.InitCommands.Cast<string>().ToList().ForEach(_serialPort.WriteLine);
+            foreach (var name in Properties.Settings.Default.InitCommands.Cast<string>())
+            {
+                Console.WriteLine(name);
+                _serialPort.WriteLine(name);
+            }
 
             Thread.Sleep(1500);
 
@@ -107,22 +113,25 @@ namespace NovAtelLogReader.Readers
 
             while (!_cts.IsCancellationRequested)
             {
+                //_logger.Debug("Start read cycle");
+                
                 Array.Resize(ref buffer, HeaderLength);
                 Array.Clear(buffer, 0, HeaderLength);
 
-                if (_serialPort.Read(buffer, 0, 1) > 0 && buffer[0] != 0xaa) continue;
-                if (_serialPort.Read(buffer, 1, 1) > 0 && buffer[1] != 0x44) continue;
-                if (_serialPort.Read(buffer, 2, 1) > 0 && buffer[2] != 0x12) continue;
-                if (_serialPort.Read(buffer, 3, 1) > 0 && buffer[3] != HeaderLength) continue;
+                if (Check(buffer, 0, 0xaa)) continue;
+                if (Check(buffer, 1, 0x44)) continue;
+                if (Check(buffer, 2, 0x12)) continue;
+                if (Check(buffer, 3, HeaderLength)) continue;
 
                 _messageCounter++;
 
-                _serialPort.Read(buffer, 4, HeaderLength - 4);
+                _serialPort.BlockingRead(buffer, 4, HeaderLength - 4);
                 dataLength = BitConverter.ToUInt16(buffer, 8);
 
                 Array.Resize(ref buffer, HeaderLength + dataLength);
-                _serialPort.Read(buffer, HeaderLength, dataLength);
-                _serialPort.Read(crc, 0, 4);
+
+                _serialPort.BlockingRead(buffer, HeaderLength, dataLength);
+                _serialPort.BlockingRead(crc, 0, 4);
 
                 if (Util.CalculateBlockCRC32(buffer) == BitConverter.ToUInt32(crc, 0))
                 {
@@ -135,12 +144,40 @@ namespace NovAtelLogReader.Readers
             }
         }
 
+
+        private bool Check(byte[] buffer, int offset, int symbol)
+        {
+            _serialPort.BlockingRead(buffer, offset, 1);
+            
+            if (buffer[offset] == symbol) return false;
+            //_logger.Debug($"Wait {symbol} but received {buffer[offset]}");
+            return true;
+        }
+
         public static void GetAvailablePortNames()
         {
             Console.WriteLine("The following serial ports were found:");
             foreach (var port in SerialPort.GetPortNames())
             {
                 Console.WriteLine(port);
+            }
+        }
+    }
+
+    internal static class SerialPortReader
+    {
+        public static void BlockingRead(this SerialPort port, byte[] buffer, int offset, int count)
+        {
+            while (count > 0)
+            {
+                // SerialPort.Read() blocks until at least one byte has been read, or SerialPort.ReadTimeout milliseconds
+                // have elapsed. If a timeout occurs a TimeoutException will be thrown.
+                // Because SerialPort.Read() blocks until some data is available this is not a busy loop,
+                // and we do NOT need to issue any calls to Thread.Sleep().
+
+                int bytesRead = port.Read(buffer, offset, count);
+                offset += bytesRead;
+                count -= bytesRead;
             }
         }
     }
